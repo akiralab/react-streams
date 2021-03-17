fc5b6c03e26d71a73d80c61abfdb510018a47ac0
Akira Kawai <akirakawai@enoMacBook-Pro.local>
2021-03-18 07:16:46 +0900

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+render(<App />, document.querySelector('#root'));
