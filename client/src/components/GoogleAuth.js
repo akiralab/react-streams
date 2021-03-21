e0ae0f75f511f8d780b92b662120e92542e9d1c1
Akira Kawai <akirakawai@enoMacBook-Pro.local>
2021-03-21 12:38:47 +0900

    added issues #315 redux-setup

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -23,10 +23,10 @@ class GoogleAuth extends React.Component {
   this.setState({ isSignedIn: this.auth.isSignedIn.get() });
  };
 
- onSignIn = () => {
+ onSignInClick = () => {
   this.auth.signIn();
  };
- onSignOut = () => {
+ onSignOutClick = () => {
   this.auth.signOut();
  };
 
@@ -35,14 +35,14 @@ class GoogleAuth extends React.Component {
    return <div>I dont know if we are signed in</div>;
   } else if (this.state.isSignedIn) {
    return (
-    <button onClick={this.onSignOut} className="ui red google button">
+    <button onClick={this.onSignOutClick} className="ui red google button">
      <i className="google icon" />
      Sign Out
     </button>
    );
   } else {
    return (
-    <button onClick={this.onSignIn} className="ui red google button">
+    <button onClick={this.onSignInClick} className="ui red google button">
      <i className="google icon" />
      Sign In with Google
     </button>
